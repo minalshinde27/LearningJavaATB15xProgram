8db721733f6894c799a69b91cc7d0542796e735d
minalshinde27 <minalshinde27@gmail.com>
2026-01-06 07:57:00 +0530

    Printf program added

--- src/Ex_01_JavaBasics/Lab001_HelloWorld.java
+++ src/Ex_01_JavaBasics/Lab001_HelloWorld.java
@@ -1,4 +1,11 @@
 package Ex_01_JavaBasics;
 
 public class Lab001_HelloWorld {
+    public static void main (String[] args){
+        System.out.print("After executing 'print' command ");
+        System.out.print("Same Line is continued");
+
+        System.out.println("Now execute 'println'");
+        System.out.print("At new Line after executing println");
+    }
 }
