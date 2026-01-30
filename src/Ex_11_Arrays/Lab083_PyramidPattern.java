2979aa7a1fe15e0af49ca6c2863caf964a775ece
minal <minal.shinde27@gmail.com>
2026-01-30 22:08:53 +0530

    StarPyramidPattern

--- src/Ex_11_Arrays/Lab083_PyramidPattern.java
+++ src/Ex_11_Arrays/Lab083_PyramidPattern.java
@@ -1,5 +1,23 @@
 package Ex_11_Arrays;
 
-public class Lab083_PyramidPattern {
+import java.util.Scanner;
 
+public class Lab083_PyramidPattern {
+    public static void main() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of stars for Pyramid");
+        int number = scanner.nextInt();
+
+        for(int row=1; row<=number; row++){
+            for(int space=1; space<=number-row; space++){
+                System.out.print(" ");
+            }
+            for(int star=1; star<=row; star++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
 }
