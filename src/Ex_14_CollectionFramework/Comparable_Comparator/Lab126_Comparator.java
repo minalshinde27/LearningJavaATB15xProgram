2a35196be17266bffefc9790314e5f894cc14bcd
minal <minal.shinde27@gmail.com>
2026-02-18 18:55:04 +0530

    Upto 14Feb2026

--- src/Ex_14_CollectionFramework/Comparable_Comparator/Lab126_Comparator.java
+++ src/Ex_14_CollectionFramework/Comparable_Comparator/Lab126_Comparator.java
@@ -8,13 +8,26 @@ import java.util.List;
 public class Lab126_Comparator {
     public static void main() {
         List<Employee> employeeList = new ArrayList<>();
-        employeeList.add(new Employee(2,"Emp2",2000));
+        employeeList.add(new Employee(2,"Emp12",2000));
         employeeList.add(new Employee(3,"Emp3",3000));
-        employeeList.add(new Employee(1,"Emp1",1000));
+        employeeList.add(new Employee(1,"Emp1",4000));
 
+        //Original
         System.out.println(employeeList);
+
+        //Using Comparator for 'id'
         Comparator<Employee> idComparator = (e1, e2) -> e1.id - e2.id;
         Collections.sort(employeeList,idComparator);
+        System.out.println(employeeList);
+
+        //Using Comparator for 'name'
+        Comparator<Employee> nameComparator = (Employee e1, Employee e2) -> e1.name.compareTo(e2.name);
+        Collections.sort(employeeList, nameComparator);
+        System.out.println(employeeList);
+
+        //Using Comparator for 'salary'
+        Comparator <Employee> salaryComparator = (Employee e1, Employee e2) -> e1.salary - e2.salary;
+        Collections.sort(employeeList, salaryComparator);
         System.out.println(employeeList);
     }
 }
